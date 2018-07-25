b20ec069e61e79f7f43251f7c36205f6bc08d8cb
Azhar Shah <azharshahstl@yahoo.com>
2018-07-25 18:00:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,58 @@
-# code here!
+require 'pry'
+class School
+  
+  
+  
+  
+  
+  def initialize(name)
+    @roster = {}
+  end
+  
+  def roster=(roster)
+    @roster = roster 
+  end 
+  
+  def roster
+    @roster
+  end
+  
+  
+  
+  def add_student=(name, grade)
+    @name = name
+    @grade = grade 
+  end
+  
+  def add_student(name, grade)
+   # binding.pry
+     @roster == {} 
+     @roster[grade] = []
+     @roster[grade] << name
+     binding.pry
+     @roster
+   
+  end
+  
+  def grade=(grade)
+    @grade = grade
+  end
+  
+  def grade(level)
+    @grade
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
